c1feaa374b43b5e1d09dcba51bda85485db4b7ff
Melih <melihkotman@gmail.com>
2024-11-20 23:56:58 +0300

    Duzenlemeler ve revizeler oldu

--- haskell_sinav_tekrari.hs
+++ haskell_sinav_tekrari.hs
@@ -5,82 +5,81 @@ import Data.List
 
 
 
-{-
--FromIntegral: Integer ve Int arasinda donusum yapar
-a :: Int
-a = 5
-b :: Int
-b = 3
+
+--FromIntegral: Integer ve Int arasinda donusum yapar
+ar :: Int
+ar = 5
+bba :: Int
+bba = 3
 f :: Double
-f = fromIntegral a / fromIntegral b
+f = fromIntegral ar / fromIntegral bba
 
 main :: IO ()
 main = do 
-    printf "a=%d\nb=%d\na/b=%f\n" a b f
+    printf "a=%d\nb=%d\na/b=%f\n" ar bba f
     -- a = 5 b = 3 a/b = 1.6666666666666667
 
 --Mod: Modulus islemi yapar yani kalanini verir
-a :: Int
-a = 5
-b :: Int
-b = 4
+as :: Int
+as = 5
+kb :: Int
+kb = 4
 
-g = mod a b
-main = print g
+g :: Int
+g = Prelude.mod as kb
+
 --1
--}
-{-
+
+
 --Yas Fonksiyonu
+yas :: (Eq a, Num a) => a -> String
 yas 18 = "Genc"
 yas 40 = "Orta"
 yas 60 = "Yasli"
 yas _ = "Bilinmeyen"
 
+yas3 :: (Eq a, Num a) => a -> String
 yas3 x
     | x == 18 = "Genc"
     | x == 40 = "Orta"
     | x == 60 = "Yasli"
     | otherwise = "Bilinmeyen"
 
-main = do
-    print (yas3 19)
-    print (yas3 40)
-    print (yas3 60)
-    print (yas 18)
-    --"Bilinmeyen"
-    --"Orta"
-    --"Yasli"
-    --"Genc"
--}
-{- 
+
+
+ 
 --Faktoriyel
+fact :: (Eq t, Num t) => t -> t
 fact 0 = 1
 fact x = x * fact (x - 1)
+c :: Integer
 c = fact 5
-main   = print c
+
 --120
--}
-{-
+
+
 --fibonnacci
+fib :: (Eq t, Num t, Num a) => t -> a
 fib x
     | x == 0 = 0
     | x == 1 = 1
     | otherwise = fib (x - 1) + fib (x - 2)
+d :: Integer
 d = fib 6
-main = print d
+
 --8
--}
+
 -- $: Fonksiyon cagirirken parantez kullanmamizi saglar
 --ornegin: topla (mod 5 3) 4 yerine topla $ mod 5 3 $ 4 yazilabilir
-{-
+
 --Fonksiyon ornekleri
+arttir :: Num a => a -> a
 arttir x = x + 1
+k :: Integer
 k = arttir 5
+l :: Double
 l = arttir 5.5
-{-main = do
-    print k
-    print l
--}
+
 --k = "Hey" --k'nin deger degistirilemez cunku statik bir veri tipidir
 
 --Lazy vs Eager Evaluation
@@ -89,27 +88,35 @@ l = arttir 5.5
 --Ornek: int i = 5; int j = f(7); printf(".....");
 --Haskell'da fonksiyona bir deger verdigimizde cozmez sadece istendigi zaman yapar
 
+faktoriyel :: (Eq t, Num t) => t -> t
 faktoriyel x 
     | x == 0 = 1
     | otherwise = x * faktoriyel (x - 1)
 
+y :: Integer
 y = faktoriyel 5
 --main = print y
 --120
+{-
 fib x
     | x == 1 = 1
     | x == 2 = 1
     | otherwise = fib (x - 1) + fib (x - 2)
-
+-}
+x :: Integer
 x = 0
+kapi :: (Eq a1, Num a1, Num a2) => a1 -> a2
 kapi x 
     | x == 0 =  1
     | x == 1 =  0
     | otherwise = 0
 
-d = kapi 1
-f = kapi 0
-e = kapi 12
+ö :: Integer
+ö = kapi 1
+ç :: Integer
+ç = kapi 0
+ğ :: Integer
+ğ = kapi 12
 {-main = do
     print d
     print f
@@ -131,16 +138,8 @@ p = hesapMakinesi 5 3 "+"
 o = hesapMakinesi 5 3 "-"
 n = hesapMakinesi 5 3 "*"
 m = hesapMakinesi 5 3 "/"
-main = do
-    print p
-    print o
-    print n
-    print m
-    --8.0
-    --2.0
-    --15.0
-    --1.6666666666666667
--}
+
+
 
 --ghc haskell'in derleyicisidir ve bu derleyecide bircok liste islemi yapabiliriz ayni zamanda haskellde de yapilir ayni islemler bu islemleri gorelim
 
@@ -157,20 +156,16 @@ main = do
 --kalan (x:xs) = (mod x 2 == 1) && (kalan xs)
 --list = kalan [1,3,4]
 --list2 = kalan [1,3,5]
-{-main = do
-    print list
-    --False
-    print list2
-    --True
--}
-{-
+
+
+
 -- : operatoru listeye soldan eleman ekler 
 3 : [] = [3]
 3 : 7: [1,2] = [3,7,1,2]
 -- ++ operatoru iki listeyi birlestirir
 [1,2] ++ [3,4] = [1,2,3,4]
 -- .. operatoru aralik belirler
-[1..5] = [1,2,3,4,5]
+{-[1..5] = [1,2,3,4,5]
 [1,3,5..15] = [1,3,5,7,9,11,13,15]
 [1,3..15] = [1,3,5,7,9,11,13,15]
 [1,3..15] = [1,3,5,7,9,11,13,15]
@@ -181,39 +176,50 @@ main = do
 --farkli tiplerde elemanlar ekleyebiliriz
 --'a' : [1,2] = ['a',1,2]
 --length: Listenin uzunlugunu verir
+length :: (Eq a1, Num a1, Num a2) => [a1] -> a2
 length [1,2,3,4] = 4
 length [] = 0
 --length [1..5] = 5
 --reverse: Listeyi ters cevirir
+reverse :: (Eq a1, Num a1, Num a2) => [a1] -> [a2]
 reverse [1,2,3,4] = [4,3,2,1]
 --null: Listenin bos olup olmadigini kontrol eder
+null :: (Eq a, Num a) => [a] -> Bool
 null [] = True
 null [1] = False
 null [1,2] = False
 --head: Listenin ilk elemanini verir ve liste olmadan sonuc çikarir
+head :: (Eq a1, Num a1, Num a2) => [a1] -> a2
 head [1,2,3] = 1 
 --tail: Listenin ilk elemanini cikarir ve geri kalanini verir
+tail :: (Eq a1, Num a1, Num a2) => [a1] -> [a2]
 tail [1,2,3] = [2,3]
 --last: Listenin son elemanini verir ayni head gibi liste olmadan sonuc cikarir
+last :: (Eq a1, Num a1, Num a2) => [a1] -> a2
 last [1,2,3] = 3
 --init: Listenin son elemanini cikarir ve geri kalanini verir
+init :: (Eq a1, Num a1, Num a2) => [a1] -> [a2]
 init [1,2,3] = [1,2]
 --take: Belirli bir sayida elemani alir
+take :: (Eq a1, Eq a2, Num a1, Num a2, Num a3) => a1 -> [a2] -> [a3]
 take 2 [1,2,3,4] = [1,2]
 --drop: Belirli bir sayida elemani cikarir ve sol kisimdan geri kalanini verir
 drop :: (Eq a1, Eq a2, Num a1, Num a2, Num a3) => a1 -> [a2] -> [a3]
 drop 2 [1,2,3,4] = [3,4]
 --elem: Elemanin listede olup olmadigini kontrol eder
+elem :: (Eq a1, Eq a2, Num a1, Num a2) => a1 -> [a2] -> Bool
 elem 2 [1,2,3,4] = True
 elem 7 [1,2,3,4] = False
 
 --7 `elem` [1,2,3,4] = False
 7 `elem` [1,2,3,4,7] = True
 --mod: Modulus islemi yapar
+mod :: (Eq a1, Eq a2, Num a1, Num a2, Num a3) => a1 -> a2 -> a3
 mod 12 5 = 2
 mod 10 5 = 0
 mod 22 11 = 0
 --product: Listenin elemanlarini carpip sonucu verir
+product :: (Eq a1, Num a1, Num a2) => [a1] -> a2
 product [1,2,3] = 6
 product [1,2,3,4] = 24
 --bu islemleri ic ice kullanabiliriz
@@ -245,42 +251,52 @@ x = 1
 y = 2
 rest = [3,4,5,6,7,8,9,10]
 -}
-{-
+
 -- listeler ile fonksiyonlar
+uzunluk :: Num a1 => [a2] -> a1
 uzunluk []  = 0
 uzunluk (x:xs) = 1 + uzunluk xs
 --main = print (uzunluk [1,2,3,4,5]) --5
+tersCevir :: [a] -> [a]
 tersCevir [] = []
 tersCevir [x] = [x]
-tersCevir (x:xs) = tersCevir xs ++ [x]
+tersCevir (x:xs) = tersCevir xs Data.List.++ [x]
 --main = print (tersCevir [1,2,3,4,5])--[5,4,3,2,1]
+bos :: [a] -> Bool
 bos [] = True
 bos (x:xs) = False
 --main = print (bos [1,2,3,4,5]) --False
+nncinciEleman :: (Eq t, Num t) => [a] -> t -> a
 nncinciEleman (x:xs) 0 = x
 nncinciEleman (x:xs) n = nncinciEleman xs (n-1)
 --main = print (nncinciEleman [1,2,3,4,5] 2) --3
+enBastaki :: [a] -> a
 enBastaki (x:xs) = x
 --main = print (enBastaki [1,2,3,4,5]) --1
+enSondaki :: [a] -> a
 enSondaki [x] = x
 enSondaki (x:xs) = enSondaki xs
 --main = print (enSondaki [1,2,3,4,5]) --5
+sondakiHaricHepsi :: [a] -> [a]
 sondakiHaricHepsi [x] = []
 sondakiHaricHepsi (x:xs) = x : sondakiHaricHepsi xs
 --main = print (sondakiHaricHepsi [1,2,3,4,5]) --[1,2,3,4]
+bastanAl :: (Eq a1, Num a1) => [a2] -> a1 -> [a3]
 bastanAl l 0 = []
 bastanAl [] n = []
 bastan (x:xs) n = x : bastan xs (n-1)
 --main = print (bastan [1,2,3,4,5] 2) --[1,2]
 
+elemanMi :: Eq t => t -> [t] -> Bool
 elemanMi x [] = False
 elemanMi x (y:ys)
     | x == y = True
     | otherwise = elemanMi x ys
 --main = print (elemanMi 3 [1,2,3,4,5]) --True
 --main = print (elemanMi 6 [1,2,3,4,5]) --False
-enBuyuk [x] = x
-enBuyuk (x:y:xs)
+enBuyuko :: [Int] -> Int
+enBuyuko [x] = x
+enBuyuko (x:y:xs)
     | x < y = enBuyuk (y:xs)
     | otherwise = enBuyuk (x:xs)
 --main = print (enBuyuk [1,2,3,4,5]) --5
@@ -289,6 +305,7 @@ toplam :: Num a1 => [a2] -> a1
 toplam [] = 0
 --toplam (x:xs) = x + (toplam xs)
 --main = print (toplam [1,2,3,4,5]) --15
+palindromMu :: Eq a => [a] -> Bool
 palindromMu x = x == tersCevir x --burada sonucu bool olarak dondurur cunku == operatorunu kullandik
 --main = print (palindromMu [1,2,3,2,1]) --True
 --main = print (palindromMu [1,2,3,4,5]) --False
@@ -296,6 +313,7 @@ carpim [x] = x
 carpim (x:xs) = x * carpim xs
 --main = print (carpim [1,2,3,4,5]) --120
 
+siralimi :: Ord a => [a] -> Bool
 siralimi [] = True
 siralimi [x] = True
 siralimi (x:y:xs)
@@ -311,19 +329,24 @@ birKezSirala (x:y:xs)
 --main = print (birKezSirala [1,2,3,5,4]) --[1,2,3,4,5]
 --main = print (birKezSirala [5,4,3,2,1]) --[4,3,2,1,5]
 --burada bir kez siraladik ve sonucu verdi yani calisma mantigi soyle 
-{-
+
+sirala :: Ord a => [a] -> [a]
 sirala l 
     | siralimi l = l
     | otherwise = sirala (birKezSirala l)
 --main = print (sirala [1,2,3,5,4]) --[1,2,3,4,5]
 --main = print (sirala [5,4,3,2,1]) --[1,2,3,4,5]
--}
+
 --Haskell Son Ornekler
+topla :: Num a => a -> a -> a
 topla a b = a + b
+ucleTopla :: Integer -> Integer
 ucleTopla = topla 3
 
 --map: Fonksiyonu listeye uygular
+karesi :: Num a => a -> a
 karesi x = x ^ 2
+myMap :: (t -> a) -> [t] -> [a]
 myMap f [] = []
 myMap f (x:xs) = f x : myMap f xs
 
@@ -332,13 +355,17 @@ myMap f (x:xs) = f x : myMap f xs
 --foldr: Listeyi bir degerde toplar(sağdan sola)
 --foldl: Listeyi bir degerde toplar(soldan saga)
 
-toplam = foldr (+) 0 [1,2,3] -- 1 + (2 + (3 + 0)) => 1 + (2 + 3) => 1 + 5
+toplamko :: Integer
+toplamko = foldr (+) 0 [1,2,3] -- 1 + (2 + (3 + 0)) => 1 + (2 + 3) => 1 + 5
 
+hepsiDogruMu :: Bool
 hepsiDogruMu = foldr (&&) True [True, 5 == 7] -- True && (5 == 7 && True)
 --main = print hepsiDogruMu --False cunku 5 == 7 false
-fact x = foldr (*) 1 [1..x]
+facto :: (Num b, Enum b) => b -> b
+facto x = foldr (*) 1 [1..x]
 tersSirala :: [Int] -> [Int]
-tersSirala = reverse . sort
+tersSirala = Data.List.reverse . sort
+tersSirali :: [Int]
 tersSirali = tersSirala [5, 1, 2]
 --main = print tersSirali --[5,2,1]
 -- . operatoru fonksiyonlari birlestirir
@@ -347,20 +374,25 @@ tersSirali = tersSirala [5, 1, 2]
 -- foldr fun a [x1, x2, ..., xn] = fun x1 (fun x2 (fun x3 (... fun xn a))) --sağdan sola
 -- foldl fun a [x1, x2, ..., xn] = fun (... (fun (fun a x1) x2) ...) xn
 --soldan saga
+ucArtiBes :: Integer
 ucArtiBes = (+) 3 5
 --ucArtiBes = 8
+toplamlar :: Integer
 toplamlar = foldr (+) 0 [1, 2, 3] -- 1 + (2 + (3 + 0))
 --toplamlar = 6
 
 
+yedilerinSayisi :: Integer
 yedilerinSayisi = foldr (\x sayi -> if x == 7 then sayi + 1 else sayi) 0 [3, 7, 1, 7, 2]
 -- f 3 (f 7 (f 1 (f 7 (f 2 0))))
 --2 cikar
+carpimlar :: Integer
 carpimlar = foldl (*) 1 [3, 5, 7] -- ((1 * 3) * 5) * 7
 
+tersi :: [Integer]
 tersi = foldl (\acc x -> x : acc) [] [1, 2, 3] -- f (f (f [] 1) 2) 3
 --acc burada bos liste olur ve x degeri 1 olur ve 1:[] olur ve 1 olur sonra 2 olur ve 2:1:[] olur ve 2:1 olur ve 3 olur ve 3:2:1:[] olur ve [3,2,1] olur
-main = do 
+{-main = do 
     print "topla:"
     print (topla 3 5)
     print "ucletopla"
@@ -373,12 +405,12 @@ main = do
     --burada filter fonksiyonu ile liste icindeki elemanlari filtreleyip bize true olanlari verir misal 4'un katlarini 1-50 arasinda bulmak istedik ve True False seklinde verdik
     print (filter (\x -> x `elem` ['a'..'z'] || x  `elem` ['A'..'Z'] ) ['1', 'S','a','5', 'b'])
     --bu yukaridaki fonksiyon eger liste icindeki eleman a-z veya A-Z araliginda ise true dondurur
-    {-
+    
     print "map"
     print (map karesi [1,2,3])
     print "myMap"
     print (myMap karesi [1,2,3])
-
+-}
 enBuyuk :: [Int] -> Int
 -- Değiştirilebilir alan 0'ın başlangıcı
 enBuyuk [] = 0
@@ -386,7 +418,7 @@ enBuyuk [a] = a
 enBuyuk [a, b] = if a < b then b else a
 enBuyuk (x:y:xs) = if x > y then enBuyuk (x:xs) else enBuyuk (y:xs)
 -- Değiştirilebilir alan 0'ın bitişi
-a = enBuyuk [1,2,-5,3,15,20]
+aa = enBuyuk [1,2,-5,3,15,20]
 --main = print a
 --20
 ucluFibo :: Int -> Int 
@@ -397,7 +429,8 @@ ucluFibo x
   | x == 3 = 1
   | otherwise = ucluFibo(x-1) + ucluFibo(x-2) + ucluFibo(x-3)
 -- Değiştirilebilir alan 1'in bitişi
-b = ucluFibo 6
+q :: Int
+q = ucluFibo 6
 --main = print b
 -- 9 soyle yapar 
 --ucluFibo(6) = ucluFibo(5) + ucluFibo(4) + ucluFibo(3) = 5 + 3 + 1 = 9
@@ -411,24 +444,22 @@ ortasi [x] = x
 ortasi [x, y] = (x + y) / 2
 ortasi (x:xs) = ortasi (Data.List.init xs)
 -- Değiştirilebilir alan 2'nin bitişi
-c = ortasi [1,2,3,4,5]
-d = ortasi [1,2,3]
-e = ortasi [1,2]
-f = ortasi [1,2,3,4,5,6,7,8,9,10]
-{-main = do
-    print c
-    print d
-    print e
-    print f
---ortasi 1 2 3 4 5
--- ortasi 2 3 4
--- ortasi 3
--}
+rr :: Double
+rr = ortasi [1,2,3,4,5]
+dd :: Double
+dd= ortasi [1,2,3]
+ee :: Double
+ee = ortasi [1,2]
+ff :: Double
+ff = ortasi [1,2,3,4,5,6,7,8,9,10]
+
+
 tamBolenler :: Int -> [Int]
 -- Değiştirilebilir alan 3'ün başlangıcı
 tamBolenler x = [y | y <- [1..x], x `Prelude.mod` y == 0]
 -- Değiştirilebilir alan 3'ün bitişi
-g = tamBolenler 12
+gas :: [Int]
+gas = tamBolenler 12
 --main = print g
 --[1,2,3,4,6,12]
 
@@ -436,8 +467,7 @@ buyukleriBul :: Int -> [Int] -> [Int]
 -- Değiştirilebilir alan 4'ün başlangıcı
 buyukleriBul a = filter (>a)
 -- bu fonksiyon 5'ten buyuk olanlari filtreler
-p = buyukleriBul 5 [1,2,3,4,5,6,7,8,9,10]
-main = print p
---[6,7,8,9,10]
--}
--}
+po :: [Int]
+po = buyukleriBul 5 [1,2,3,4,5,6,7,8,9,10]
+
+
